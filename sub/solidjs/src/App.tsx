88a51797d79939bf6c784957d20234040b4abf77
赵康 <zhaokang@udesk.cn>
2022-09-09 16:40:03 +0800

    fix: vite静态资源加载

--- sub/solidjs/src/App.tsx
+++ sub/solidjs/src/App.tsx
@@ -1,14 +1,15 @@
 import type { Component } from "solid-js";
 import { Routes, Route } from "@solidjs/router";
 import Count from "./pages/Count";
-import logo from "./logo.svg";
 import styles from "./App.module.css";
 
 const App: Component = () => {
+  const src = new URL('./assets/logo.svg', import.meta.url).href;
+
   return (
     <div class={styles.App}>
       <header class={styles.header}>
-        <img src={logo} class={styles.logo} alt="logo" />
+        <img src={src} class={styles.logo} alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
--- sub/umi4/src/pages/index.tsx
+++ sub/umi4/src/pages/index.tsx
@@ -1,12 +1,9 @@
-import yayJpg from '../assets/yay.jpg';
+import React from "react";
 
 export default function HomePage() {
   return (
     <div>
       <h2>Yay! Welcome to umi!</h2>
-      <p>
-        <img src={yayJpg} width="388" />
-      </p>
       <p>
         To get started, edit <code>pages/index.tsx</code> and save to reload.
       </p>
